1e27b1da2a89d6876dfdd8fe62be6eb1cf884399
Andhika Prasetya <andhikapr081@gmail.com>
2022-07-25 21:02:38 +0700

    make computed for answer

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -7,6 +7,24 @@
 <script>
 export default {
   props: ['results', 'totalCorrect'],
+  computed: {
+    resultIndex() {
+      let index = 0;
+
+      // loop through results array
+      // e = represent the key of the array
+      // i = represent the index of the array
+      this.results.forEach((e, i) => {
+        // if totalCorrect variabel is bigger than e.min and smaller than e.max
+        if (e.min <= this.totalCorrect && e.max >= this.totalCorrect) {
+          // set the "index" variabel value to index of the array
+          index = i;
+        }
+      });
+
+      return index;
+    },
+  },
 };
 </script>
 <style lang=""></style>
